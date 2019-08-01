66d816ce1ef9f2cae3e998493a3b635538f71ed4
C-Rodg <C-Rodg@users.noreply.github.com>
2019-07-31 20:36:59 -0700

    Starting to inclue async storage

--- src/screens/StartInitializingScreen.js
+++ src/screens/StartInitializingScreen.js
@@ -1,24 +1,53 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, Component } from 'react';
 import {
 	SafeAreaView,
 	StyleSheet,
 	ScrollView,
 	View,
 	Text,
-	StatusBar
+	StatusBar,
+	AsyncStorage
 } from 'react-native';
 
-const StartInitializingScreen = () => {
-	return (
-		<Fragment>
-			<StatusBar barStyle="dark-content" />
-			<SafeAreaView>
-				<View>
-					<Text>Start initializing and determine app state</Text>
-				</View>
-			</SafeAreaView>
-		</Fragment>
-	);
-};
+import { goToAuth, goToHome } from './navigation';
+import { USER_KEY } from '../config/config';
+
+class StartInitializingScreen extends Component {
+	async componentDidMount() {
+		try {
+			const user = await AsyncStorage.getItem(USER_KEY);
+			console.log('user: ', user);
+			if (user) {
+				goToHome();
+			} else {
+				goToAuth();
+			}
+		} catch (err) {
+			console.log('error:', err);
+			goToAuth();
+		}
+	}
+
+	render() {
+		return (
+			<Fragment>
+				<StatusBar barStyle="dark-content" />
+				<SafeAreaView>
+					<View style={styles.container}>
+						<Text>Starting to initialize...</Text>
+					</View>
+				</SafeAreaView>
+			</Fragment>
+		);
+	}
+}
 
 export default StartInitializingScreen;
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		justifyContent: 'center',
+		alignItems: 'center'
+	}
+});
